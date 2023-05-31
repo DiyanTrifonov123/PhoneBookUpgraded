0a944cfbda8f3c7bcfa4385250fee850f9521884
DiyanTrifonov123 <diyantrifonov5@gmail.com>
2023-05-31 11:31:24 +0300

    First commit

---
+++ TelefonenUkazatel/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelefonenUkazatel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter count: ");
+            int n = int.Parse(Console.ReadLine());
+            AddressEntry[] addressEntries = new AddressEntry[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                AddressEntry address = new AddressEntry();
+                address.Input();
+                addressEntries[i] = address;
+            }
+
+            foreach (var item in addressEntries)
+            {
+                item.Output();
+            }
+        }
+    }
+}
